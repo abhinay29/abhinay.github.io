f958110cb96d91d9085533ced1518a49065dd291
Abhinay Sharma <abhinaysharma.vw@gmail.com>
2020-12-24 11:02:35 +0530

    Setup mail

---
+++ malshej/ajax.php
@@ -0,0 +1,40 @@
+<?php
+
+  $retVal = array();
+
+  if(!isset($_POST['do'])){
+    header('location: ./');
+  }else{
+    $do = $_POST['do'];
+  }
+  
+  include 'clsMail.php';
+  
+  $clsMail = new clsMail();
+  
+  switch($do) {
+    
+    case 'enq_form':
+
+        $data = [
+          'name'      =>  $_POST['name'],
+          'email'     =>  $_POST['email'],
+          'phone'     =>  $_POST['phone'],
+          'message'  =>  $_POST['message']
+        ];
+          
+        $sendEmail = $clsMail->sendEnquiryForm($data);
+
+        if($sendEmail){
+          $retVal['msg'] = 'ok';
+        }else{
+          $retVal['msg'] = 'err';
+        }
+      
+    break;
+
+  }
+
+  echo json_encode($retVal);
+
+?>
